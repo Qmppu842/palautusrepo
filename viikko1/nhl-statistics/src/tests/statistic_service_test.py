65bb11c3995eb385733eba24ed33bd3aad1a1e6a
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-11-03 23:48:05 +0200

    Dang no more time to figure these last ones...

--- viikko1/nhl-statistics/src/tests/statistic_service_test.py
+++ viikko1/nhl-statistics/src/tests/statistic_service_test.py
@@ -24,3 +24,31 @@ class TestStatisticsService(unittest.TestCase):
 
     def test_search_existing_existing(self):
         self.assertEqual((self.stats.search("Semenko")).assists, 12)
+
+    def test_team_thing(self):
+        players_of_team = self.stats.team("EDM")
+        self.assertEqual(len(players_of_team), 3)
+
+    def test_top_thing(self):
+        result = self.stats.top(2)
+        print("kala")
+        for x in result:
+            print(x.__str__())
+
+        self.assertEqual(len(result), 3)
+        
+    def test_top_thing3(self):
+        result = self.stats.top(-1)
+        self.assertEqual(len(result), 0)
+
+    def test_top_thing2(self):
+        result: list[Player] = self.stats.top(2)
+
+        self.assertEqual(result[0].name, "Gretzky")
+        self.assertEqual(result[1].team, "PIT")
+        self.assertEqual(result[2].points, 98)
+        
+        
+    def test_top_thing4(self):
+        result = self.stats.top(100)
+        self.assertEqual(len(result), 5)
