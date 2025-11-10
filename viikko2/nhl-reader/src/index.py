63a8489874a9dd634ee3042139e20299434abcf6
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-11-10 15:29:42 +0200

    Completed w2t2

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,41 @@
+import requests
 from player import Player
 
+
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players"
+    response = requests.get(url).json()
+
+    print("JSON-muotoinen vastaus:")
+    # print(response)
+
+    players = []
+
+    for player_dict in response:
+        # print(f"player_dict: {player_dict}")
+        player = Player(player_dict)
+        players.append(player)
+
+    # print("Oliot:")
+
+    for player in players:
+        # print(player)
+        pass
+
+    # where = "nationality"
+    # where_value = "FIN"
+    where = "goals"
+    where_value = "9"
+
+    collected = []
+    collected = list(filter(lambda x: str(getattr(x, where)) == where_value, players))
+
+    print("pelaajat:")
+
+    for player in collected:
+        print(player.dyna_str(where))
+        pass
+
 
 if __name__ == "__main__":
     main()
