3f2547ca1c29bb1e665f1ee96708b3309a6450aa
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-12-01 19:26:45 +0200

    Extracted methods

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -6,50 +6,66 @@ class TennisGame:
         self.player2_score = 0
 
     def won_point(self, player_name):
-        if player_name == "player1":
-            self.player1_score+= 1
+        if player_name == self.player1_name:
+            self.player1_score += 1
         else:
             self.player2_score += 1
 
     def get_score(self):
-        score = ""
-        temp_score = 0
+        score_as_text = ""
+
+        return score_as_text
+
+    def get_score1(self):
+        score_as_text = ""
 
         if self.player1_score == self.player2_score:
-            if self.player1_score == 0:
-                score = "Love-All"
-            elif self.player1_score == 1:
-                score = "Fifteen-All"
-            elif self.player1_score == 2:
-                score = "Thirty-All"
-            else:
-                score = "Deuce"
+            self.tie_names()
         elif self.player1_score >= 4 or self.player2_score >= 4:
-            minus_result = self.player1_score - self. player2_score
-
-            if minus_result == 1:
-                score = "Advantage player1"
-            elif minus_result == -1:
-                score = "Advantage player2"
-            elif minus_result >= 2:
-                score = "Win for player1"
-            else:
-                score = "Win for player2"
+            score_as_text = self.advantage()
         else:
-            for i in range(1, 3):
-                if i == 1:
-                    temp_score = self.player1_score
-                else:
-                    score = score + "-"
-                    temp_score = self.player2_score
+            score_as_text = self.something_insane(score_as_text)
+        return score_as_text
 
-                if temp_score == 0:
-                    score = score + "Love"
-                elif temp_score == 1:
-                    score = score + "Fifteen"
-                elif temp_score == 2:
-                    score = score + "Thirty"
-                elif temp_score == 3:
-                    score = score + "Forty"
+    def tie_names(self, score):
+        if score == 0:
+            score_as_text = "Love-All"
+        elif score == 1:
+            score_as_text = "Fifteen-All"
+        elif score == 2:
+            score_as_text = "Thirty-All"
+        else:
+            score_as_text = "Deuce"
+        return score_as_text
 
-        return score
+    def advantage(self):
+        minus_result = self.player1_score - self.player2_score
+
+        if minus_result == 1:
+            score_as_text = "Advantage player1"
+        elif minus_result == -1:
+            score_as_text = "Advantage player2"
+        elif minus_result >= 2:
+            score_as_text = "Win for player1"
+        else:
+            score_as_text = "Win for player2"
+        return score_as_text
+
+    def something_insane(self, score_as_text):
+        temp_score = 0
+        for i in range(1, 3):
+            if i == 1:
+                temp_score = self.player1_score
+            else:
+                score_as_text = score_as_text + "-"
+                temp_score = self.player2_score
+
+            if temp_score == 0:
+                score_as_text = score_as_text + "Love"
+            elif temp_score == 1:
+                score_as_text = score_as_text + "Fifteen"
+            elif temp_score == 2:
+                score_as_text = score_as_text + "Thirty"
+            elif temp_score == 3:
+                score_as_text = score_as_text + "Forty"
+        return score_as_text
