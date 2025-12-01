5707053bae84a1ebcbe671f3a8aea12dd4e695cf
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-12-01 23:46:57 +0200

    Heh I knew there was use for that thing.

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -16,9 +16,11 @@ class TennisGame:
         if self.player1_score == self.player2_score:
             score_as_text = self.tie_names(self.player1_score)
         elif self.player1_score >= 4 or self.player2_score >= 4:
-            score_as_text = self.advantage(self.player1_score - self.player2_score)
+            score_as_text = self.advantage_names(
+                self.player1_score - self.player2_score
+            )
         else:
-            score_as_text = self.something_insane(score_as_text)
+            score_as_text = self.other_score_names()
         return score_as_text
 
     def tie_names(self, score, score_as_text=""):
@@ -32,7 +34,7 @@ class TennisGame:
             score_as_text = "Deuce"
         return score_as_text
 
-    def advantage(self, score_difference, score_as_text=""):
+    def advantage_names(self, score_difference, score_as_text=""):
         if score_difference > 0:
             score_as_text += self.player1_name
         elif score_difference < 0:
@@ -45,36 +47,19 @@ class TennisGame:
             score_as_text = "Win for " + score_as_text
         return score_as_text
 
-    def something_insane(self, score_as_text="xx"):
-        print(f"Starting new")
-        print(f"player1_score:{self.player1_score}")
-        print(f"player2_score:{self.player2_score}")
-        temp_score = 0
-        for i in range(1, 3):
-            print(f"round: {i}")
-            print(f"score_as_text1: {score_as_text}")
-            if i == 1:
-                print(f"score_as_text2: {score_as_text}")
-                temp_score = self.player1_score
-                print(f"score_as_text3: {score_as_text}")
-            else:
-                print(f"score_as_text4: {score_as_text}")
-                score_as_text = score_as_text + "-"
-                temp_score = self.player2_score
-                print(f"score_as_text5: {score_as_text}")
+    def other_score_names(self):
+        score_names = ["Love", "Fifteen", "Thirty", "Forty"]
 
-            print(f"score_as_text6: {score_as_text}")
-            if temp_score == 0:
-                score_as_text = score_as_text + "Love"
-                print(f"score_as_text7: {score_as_text}")
-            elif temp_score == 1:
-                score_as_text = score_as_text + "Fifteen"
-                print(f"score_as_text8: {score_as_text}")
-            elif temp_score == 2:
-                score_as_text = score_as_text + "Thirty"
-                print(f"score_as_text9: {score_as_text}")
-            elif temp_score == 3:
-                score_as_text = score_as_text + "Forty"
-                print(f"score_as_text10: {score_as_text}")
-            print(f"score_as_text11: {score_as_text}")
-        return score_as_text
+        player1_score_text = ""
+        if self.player1_score < len(score_names):
+            player1_score_text = score_names[self.player1_score]
+        else:
+            player1_score_text = self.player1_score
+
+        player2_score_text = ""
+        if self.player2_score < len(score_names):
+            player2_score_text = score_names[self.player2_score]
+        else:
+            player2_score_text = self.player2_score
+
+        return f"{player1_score_text}-{player2_score_text}"
