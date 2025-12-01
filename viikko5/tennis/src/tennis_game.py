b071a3c0b2b46e5c4529820794a6cd7a83a5f66d
Qmppu842 <6827447+Qmppu842@users.noreply.github.com>
2025-12-01 23:36:02 +0200

    mehx2

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -1,6 +1,4 @@
 class TennisGame:
-    SCORE_NAMES = ["Love", "Fifteen", "Thirty", "Forty"]
-
     def __init__(self, player1_name, player2_name):
         self.player1_name = player1_name
         self.player2_name = player2_name
@@ -14,28 +12,7 @@ class TennisGame:
             self.player2_score += 1
 
     def get_score(self):
-        score_as_text = "-"
-
-        # player1_score_as_text = ""
-        if self.player1_score < len(self.SCORE_NAMES):
-            player1_score_as_text = self.SCORE_NAMES[self.player1_score]
-        else:
-            player1_score_as_text = self.player1_score
-
-        # player2_score_as_text = ""
-        if self.player2_score < len(self.SCORE_NAMES):
-            player2_score_as_text = self.SCORE_NAMES[self.player2_score]
-        else:
-            player2_score_as_text = self.player2_score
-
-        if player1_score_as_text == player2_score_as_text:
-            score_as_text = f"{player1_score_as_text}-All"
-
-        return score_as_text
-
-    def get_score1(self):
         score_as_text = ""
-
         if self.player1_score == self.player2_score:
             score_as_text = self.tie_names(self.player1_score)
         elif self.player1_score >= 4 or self.player2_score >= 4:
@@ -68,21 +45,36 @@ class TennisGame:
             score_as_text = "Win for " + score_as_text
         return score_as_text
 
-    def something_insane(self, score_as_text):
+    def something_insane(self, score_as_text="xx"):
+        print(f"Starting new")
+        print(f"player1_score:{self.player1_score}")
+        print(f"player2_score:{self.player2_score}")
         temp_score = 0
         for i in range(1, 3):
+            print(f"round: {i}")
+            print(f"score_as_text1: {score_as_text}")
             if i == 1:
+                print(f"score_as_text2: {score_as_text}")
                 temp_score = self.player1_score
+                print(f"score_as_text3: {score_as_text}")
             else:
+                print(f"score_as_text4: {score_as_text}")
                 score_as_text = score_as_text + "-"
                 temp_score = self.player2_score
+                print(f"score_as_text5: {score_as_text}")
 
+            print(f"score_as_text6: {score_as_text}")
             if temp_score == 0:
                 score_as_text = score_as_text + "Love"
+                print(f"score_as_text7: {score_as_text}")
             elif temp_score == 1:
                 score_as_text = score_as_text + "Fifteen"
+                print(f"score_as_text8: {score_as_text}")
             elif temp_score == 2:
                 score_as_text = score_as_text + "Thirty"
+                print(f"score_as_text9: {score_as_text}")
             elif temp_score == 3:
                 score_as_text = score_as_text + "Forty"
+                print(f"score_as_text10: {score_as_text}")
+            print(f"score_as_text11: {score_as_text}")
         return score_as_text
